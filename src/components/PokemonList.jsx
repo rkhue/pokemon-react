49ba909bb628ecb32243cd318d1e30fbfac0f3bd
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-20 06:33:11 -0300

    (feat) Componente da lista de pokémon

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,15 @@
+const PokemonList = ({ pokemonData }) => {
+    return (
+        <div>
+            <h1>Lista de Pokémons</h1>
+
+            <div className="pokemon-list">
+                {pokemonData.map((pokemon, index) => (
+                    <PokemonCard key={index} pokemon={pokemon} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default PokemonList;
